9dab005b9b71c98f7b4807a200977fe999f8e563
Morgado <Morgado@Morgado-PC.einet.ad.eivd.ch>
2016-05-19 20:57:20 +0200

    version final du 19.05.2016

--- afficheur.c
+++ afficheur.c
@@ -9,10 +9,35 @@ static char ascii7Segments[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 
 
 void afficheurEtablitDigits(char nombre) {
     // À implémenter...
+    
+    char d1, d2;
+    
+    if(nombre>99){          // Test si la valeur dépace 99
+        digits [0]=9;
+        digits [1]=9;
+    }
+    else {     
+        d1 = nombre / 10;   // dizaine
+        d2 = nombre % 10;   // unité
+        digits[0] = d1;     // dizaine
+        digits[1] = d2;     // unité
+    }
 }
 
 unsigned char digit(unsigned char position) {
-    // À implémenter...
+   // À implémenter...
+    // code ascii selon tableau conversion BCD
+    
+    
+    if (position == 0 && digits[0]==0){         //retourne 0 sur le digit des dizaines
+        return (0);
+    }
+    else if (position == 0){                    //retourne un code ascii sur le digit des dizaines
+        return (ascii7Segments[digits[0]]);
+    }
+   else {
+        return (ascii7Segments[digits[1]]);     //retourne un code ascii sur le digit des unitées
+    }
     return 0;
 }
 
